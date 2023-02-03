ff60c9871d3077716000652487c17235648cc9d4
Prabhjot Singh <singh.74.prabh@gmail.com>
2023-02-03 07:15:26 +0530

    basic loader added

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@ import "./general.css";
 import Landing from "./Components/Landing/Landing";
 import Navbar from "./Components/Navbar/Navbar";
 import Cursor from "./Components/Cursor/Cursor";
+import { useEffect, useState } from "react";
+import Loader from "./Components/Loader/Loader";
 
 function App() {
+	const [isLoading, setLoading] = useState(true);
+
+	useEffect(() => {
+		setLoading(false)
+	}, []);
+
 	return (
 		<>
-
+			{isLoading? <Loader /> : <></>}
 			<Navbar />
 			<Landing />
-
-      <Cursor />
+			<Cursor />
 		</>
 	);
 }
---
+++ src/Components/Loader/Loader.jsx
@@ -0,0 +1,9 @@
+import "./loader.css";
+
+export default function Loader() {
+	return(
+    <section className="loader-container">
+        <p className="loader-content">Loading...</p>
+    </section>
+    ) 
+}
